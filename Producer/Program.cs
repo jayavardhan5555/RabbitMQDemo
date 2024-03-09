da7117121df050a90f2d9acc9ccfd286727765ca
jayavardhan5555 <89379481+jayavardhan5555@users.noreply.github.com>
2024-03-09 13:02:22 +0530

    Add project files.

---
+++ Producer/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using(var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+    string msg = "Getting started with .Net core rabbitmq 9012";
+
+    var body = Encoding.UTF8.GetBytes(msg);
+
+    channel.BasicPublish("", "BasicTest", null, body);
+    Console.WriteLine("sent msg {0}...", msg);
+}
+
+Console.WriteLine("Enter to exit producer");
+Console.ReadLine();
